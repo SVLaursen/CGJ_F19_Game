f5983b89bda4558882e75bcc52b0c50bd2093287
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2019-11-22 22:59:05 +0100

    Start of Upgrades

---
+++ CGJ_F19_Level_Up/Assets/Scripts/Upgrade/Upgrade.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+public enum UpgradeType { Positive, Negative, Neutral }
+
+[CreateAssetMenu(fileName = "Upgrade", menuName = "ScriptableObjects/Upgrade", order = 1)]
+public class Upgrade : ScriptableObject
+{
+	public string title;
+	public UpgradeType upgradeType;
+
+
+	[Header("Player effects")]
+	public float moveSpeed = 0f;
+	public float vitality = 0f;
+
+	[Header("Weapon effects")]
+	public float fireRate = 0f;
+	public float fireSpread = 0f;
+
+	[Header("Projectile effects")]
+	public float range = 0f;
+	public float damage = 0f;
+	public float projectileSpeed = 0f;
+
+	public void ApplyUpgrade()
+	{
+
+
+	}
+}

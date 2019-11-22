dd57dc0974691e7393def13c469ccf894589b706
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2019-11-22 23:17:28 +0100

    Added toString, changed vitality to Int

--- CGJ_F19_Level_Up/Assets/Scripts/Upgrade/Upgrade.cs
+++ CGJ_F19_Level_Up/Assets/Scripts/Upgrade/Upgrade.cs
@@ -10,7 +10,7 @@ public class Upgrade : ScriptableObject
 
 	[Header("Player effects")]
 	public float moveSpeed = 0f;
-	public float vitality = 0f;
+	public int vitality = 0;
 
 	[Header("Weapon effects")]
 	public float fireRate = 0f;
@@ -25,5 +25,41 @@ public class Upgrade : ScriptableObject
 	{
 
 
+	}
+
+	public string toString()
+	{
+		string result = "";
+
+		if(moveSpeed != 0)
+		{
+			result += "Movement speed " + (moveSpeed > 0 ? $"+{moveSpeed}" : $"{moveSpeed}") + "\n";
+		}
+		if (vitality != 0)
+		{
+			result += "Vitality " + (vitality > 0 ? $"+{vitality}" : $"{vitality}") + "\n";
+		}
+		if (fireRate != 0)
+		{
+			result += "Fire rate " + (fireRate > 0 ? $"+{fireRate}" : $"{fireRate}") + "\n";
+		}
+		if (fireSpread != 0)
+		{
+			result += "Fire spread " + (fireSpread > 0 ? $"+{fireSpread}" : $"{fireSpread}") + "\n";
+		}
+		if (range != 0)
+		{
+			result += "Projectile range " + (range > 0 ? $"+{range}" : $"{range}") + "\n";
+		}
+		if (damage != 0)
+		{
+			result += "Projectile Damage " + (damage > 0 ? $"+{damage}" : $"{damage}") + "\n";
+		}
+		if (projectileSpeed != 0)
+		{
+			result += "Projectile Speed " + (projectileSpeed > 0 ? $"+{projectileSpeed}" : $"{projectileSpeed}") + "\n";
+		}
+
+		return result;
 	}
 }
